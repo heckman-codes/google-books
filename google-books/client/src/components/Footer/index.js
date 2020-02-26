29bd58d0ba87817b00ad10c9e54029616fcb12b7
heckman-codes <liamheckmancode@gmail.com>
2020-02-25 20:44:19 -0800

    initial files

---
+++ google-books/client/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        Liam Heckman 2020
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
